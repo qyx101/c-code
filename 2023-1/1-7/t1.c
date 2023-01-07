e205423803e3ea2a3b262c7217bad9a2a38b168b
dasldjq <qyx_1001@163.com>
2023-01-07 20:35:30 +0800

    1-7

---
+++ 2023-1/1-7/t1.c
@@ -0,0 +1,39 @@
+// 杨辉三角
+
+#include <stdio.h>
+int main()
+{
+    int arr[30][30] = {0};
+    int n = 0;
+    scanf("%d",&n);
+    int i = 0;
+    for (i = 0; i < n; i++)
+    {
+        int j = 0;
+        for (j = 0; j <= i; j++)
+        {
+            if (j == 0)
+            {
+                arr[i][j] = 1;
+            }
+            if (j == i)
+            {
+                arr[i][j] = 1;
+            }
+            if (j > 0 && i > 1)
+            {
+                arr[i][j] = arr[i-1][j-1] + arr[i-1][j];
+            }
+        }
+    }
+
+    for (i = 0; i < n; i++)
+    {
+        int j = 0;
+        for (j = 0; j <= i; j++)
+        {
+            printf("%5d",arr[i][j]);
+        }        
+        printf("\n");
+    }
+}
