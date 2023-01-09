2c29a409f6517a144e447bbbe891708fed4e7294
dasldjq <qyx_1001@163.com>
2023-01-09 09:13:23 +0800

    1-8

---
+++ 2023-1/1-8/t1.c
@@ -0,0 +1,47 @@
+#define _CRT_SECURE_NO_DEPRECATE
+#include <stdio.h>
+int main()
+{
+	//int a = 0x11223344;
+	///*int* pa = &a;*/
+	////char* pa = &a;
+	//short* pa = &a;
+	//*pa = 0;
+	//printf("%d\n", a);
+
+	/*int a = 0x11223344;
+	char* pa = &a;
+	for (int i = 0; i < 4; i++)
+	{
+		*pa = 0;
+		pa++;
+	}*/
+
+	/*int a = 1;
+	int* pi = &a;
+	char* pa = &a;
+
+	printf("%p\n", pi);
+	printf("%p\n", pa);
+	printf("%p\n", pi+1);
+	printf("%p\n", pa+1);*/
+
+	/*int arr[10] = { 0 };
+	printf("%d\n", &arr[9] - &arr[0]);*/
+
+	/*int len(char* str)
+	{
+		char* start = str;
+		while (*str != '\0')
+		{
+			str++;
+		}
+		return str - start;
+	}
+	char arr[] = "abcdef";
+	int res = len(arr);
+	printf("%d\n", res);
+	return 0;*/
+	
+}
+
