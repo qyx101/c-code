37b1d9114f75fbaaa095605b905e6a4273a1fc24
dasldjq <qyx_1001@163.com>
2023-01-12 12:50:59 +0800

    1-12

---
+++ 2023-1/1-12/t1.c
@@ -0,0 +1,87 @@
+#define _CRT_SECURE_NO_DEPRECATE
+//#include <stdio.h>
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	//// arr
+//	//printf("%p\n", arr);
+//	//printf("%p\n\n", arr + 1);
+//
+//	//// &arr
+//	//printf("%p\n", &arr);
+//	//printf("%p\n", &arr + 1);
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int(*pa)[10] = &arr;
+//
+//}
+
+//#include <stdio.h>
+//
+//void print(int(*pa)[5], int row, int col)
+//{
+//	int i = 0;
+//	for (i = 0; i < row; i++)
+//	{
+//		int j = 0;
+//		for (j = 0; j < col; j++)
+//		{
+//			printf("%d ", (*(pa + i))[j]);
+//		}
+//		printf("\n");
+//	}
+//}
+//
+//int main()
+//{
+//	int arr[3][5] = { {1,2,3,4,5},{2,3,4,5,6},{3,4,5,6,7} };
+//	print(arr, 3, 5);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int arr[3] = { 1,2,3 };
+//	for (int i = 0; i < 3; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//}
+
+//#include <stdio.h>
+//
+//int sum(int x, int y)
+//{
+//	return x + y;
+//}
+//
+//int main()
+//{
+//	int (*pf)(int, int) = &sum;
+//	int res = (*pf)(1, 2);
+//	printf("%d\n", res);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{	
+//	int n = 0;
+//	scanf("%d", &a);
+//	int arr[3][5] = {
+//		{1,2,3,4,5},
+//		{2,3,4,5,6},
+//		{3,4,5,6,7}
+//	};
+//	for (int i = 0; i < 3; i++)
+//	{
+//		for ()
+//	}
+//}
+
+//
