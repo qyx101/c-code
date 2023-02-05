f65bc2884cf8c156d70968d70f95b9cf0a8b04dc
dasldjq <qyx_1001@163.com>
2023-02-05 18:53:11 +0800

    2-5

--- 2023-2/2-5/fputc.c
+++ 2023-2/2-5/fputc.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #pragma warning(disable:4996)
 
-// fopen
+
 int main()
 {
 	FILE* pc = fopen("E:\\项目\\VS代码\\Project16\\test1.txt", "r");
@@ -14,6 +14,17 @@ int main()
 	else
 	{
 		printf("打开成功\n"); 
+
+		// 写入文件
+		//for (char i = 'a'; i < 'z'; i++)
+		//{
+		//	fputc(i, pc);
+		//}
+		// 
+		
+		//读文件
+		int ret = fgetc(pc);
+		printf("%c\n",ret);
 		fclose(pc);
 		pc = NULL;
 	}
