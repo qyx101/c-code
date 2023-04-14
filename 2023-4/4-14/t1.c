77938f7c4dfee2dcac4c054941ab884adceed151
qyx101 <qyx_1001@163.com>
2023-04-14 20:41:48 +0800

    4-14

---
+++ 2023-4/4-14/t1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// 域的概念
+//// 全局域
+//int a = 0;
+//
+//// 命名空间域
+//namespace bit
+//{
+//	int a = 1;
+//}
+//
+//// 局部域
+//int main()
+//{
+//	int a = 2;
+//	printf("%d ", a); 
+//	printf("%d ", ::a);
+//	printf("%d ", bit::a);
+//}
